04aaa10402c8e33cfdf89a29882d65d5ec2a638c
yevhen-m <yvhn.yvhn@gmail.com>
2016-03-20 18:20:15 +0200

    Change ws to wss in chat.js

--- static/chat.js
+++ static/chat.js
@@ -26,7 +26,7 @@ var updater = {
     socket: null,
 
     start: function() {
-        var url = "ws://" + location.host + "/chatsocket";
+        var url = "wss://" + location.host + "/chatsocket";
         updater.socket = new WebSocket(url);
         updater.socket.onmessage = function(event) {
             updater.showMessage(event.data);
